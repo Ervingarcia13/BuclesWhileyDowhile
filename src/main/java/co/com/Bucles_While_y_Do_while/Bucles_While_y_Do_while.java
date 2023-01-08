6987b4204d8eb1ba9009c6502377ac04330732cf
Ervingarcia13 <ervingarcia13@hotmail.com>
2023-01-08 01:31:27 -0500

    Haciendo un ejemplo  de este  ejercicio Bucles While y Do while

---
+++ src/main/java/co/com/Bucles_While_y_Do_while/Bucles_While_y_Do_while.java
@@ -0,0 +1,8 @@
+package co.com.Bucles_While_y_Do_while;
+
+public class Bucles_While_y_Do_while {
+
+
+
+
+}
---
+++ src/main/java/co/com/Bucles_While_y_Do_while/Main.java
@@ -0,0 +1,17 @@
+package co.com.Bucles_While_y_Do_while;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int contador =10;
+
+        while (contador >0) {
+
+            System.out.println(" El Valor De Contador  es "  +contador);
+            contador=contador -1 ;
+
+
+        }
+
+    }
+}
